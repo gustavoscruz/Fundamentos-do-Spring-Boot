3bf6900a245a83ccc7a43c3aa31cd512c5e31cec
gustavoscruz <gustadscruz@gmail.com>
2023-11-10 09:48:35 -0300

    Created and updated controller package

--- src/main/java/br/com/gustavoscruz/controller/PrimeiraController.java
+++ src/main/java/br/com/gustavoscruz/controller/PrimeiraController.java
@@ -1,4 +1,4 @@
-package br.com.gustavoscruz.projetospringboot;
+package br.com.gustavoscruz.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/br/com/gustavoscruz/projetospringboot/ProjetoSpringbootApplication.java
+++ src/main/java/br/com/gustavoscruz/projetospringboot/ProjetoSpringbootApplication.java
@@ -2,7 +2,9 @@ package br.com.gustavoscruz.projetospringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "br.com.gustavoscruz")
 @SpringBootApplication
 public class ProjetoSpringbootApplication {
 
