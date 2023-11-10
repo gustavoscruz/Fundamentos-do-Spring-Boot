95139a6e2192e05523e770dbf7f35d54499434a9
gustavoscruz <gustadscruz@gmail.com>
2023-11-10 10:28:12 -0300

    modified ProjetoSpringbootApplication class

--- src/main/java/br/com/gustavoscruz/controller/PrimeiraController.java
+++ src/main/java/br/com/gustavoscruz/controller/PrimeiraController.java
@@ -1,7 +1,11 @@
 package br.com.gustavoscruz.controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,9 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class PrimeiraController {
     
-    @GetMapping("/primeiroMetodo")
-    public String primeiroMetodo(){
-        return "Parabéns, você criou sua primeira rota";
+    @GetMapping("/primeiroMetodo/{id}")
+    public String primeiroMetodo(@PathVariable String id){
+        return "O parâmetro é " + id;
+    }
+
+    @GetMapping("/metodoComQueryParams")
+    public String metodoComQueryParams(@RequestParam String id){
+        return "O parâmetro com metodo Query é " + id;
+    }
+
+    @GetMapping("/metodoComQueryParams2")
+    public String metodoComQueryParams2(@RequestParam Map<String, String> allParams){
+        return "O parâmetro com metodo Query 2 é " + allParams.entrySet();
     }
 
 }
